eb568b15a95167e6903faccde50d4e5c6d92ed4a
fluffydragon23 <130578123+fluffydragon23@users.noreply.github.com>
2023-12-02 12:53:54 +0800

    upgrade now has an effect

--- js/layers.js
+++ js/layers.js
@@ -15,6 +15,7 @@ addLayer("p", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+if (hasUpgrade('p', 12)) mult = mult.times(2.718)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
