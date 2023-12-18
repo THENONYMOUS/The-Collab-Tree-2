103c11811a3143212ac3a1225bcdf7a438f49c6f
jakub791 <jakubkarolmazu@gmail.com>
2023-12-18 20:45:14 +0100

    refactor: mod files loader

--- js/technical/loader.js
+++ js/technical/loader.js
@@ -1,9 +1,6 @@
 // Load files
-
-for (file in modInfo.modFiles) {
-    let script = document.createElement("script");
-    script.setAttribute("src", "js/" + modInfo.modFiles[file]);
-    script.setAttribute("async", "false");
-    document.head.insertBefore(script, document.getElementById("temp"));
+for (const filePath of modInfo.modFiles) {
+    const script = document.createElement("script");
+    script.js = `js/${filePath}`;
+    document.head.appendChild(script);
 }
-
